fcc6088c47323625c95d4cb3336e1976f6d3f7a0
gigarthan <gigajpx@gmail.com>
2018-10-01 07:16:07 +0530

    add transactional annotation

--- src/main/java/lk/sliit/csse/procurementsystem/repositories/OrderedItemsRepository.java
+++ src/main/java/lk/sliit/csse/procurementsystem/repositories/OrderedItemsRepository.java
@@ -18,11 +18,13 @@ package lk.sliit.csse.procurementsystem.repositories;
 import lk.sliit.csse.procurementsystem.models.OrderedItems;
 import lk.sliit.csse.procurementsystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
  * @author Marjan
  */
+@Transactional
 public interface OrderedItemsRepository <T extends OrderedItems> extends JpaRepository<T, Long>  {
     
 }
