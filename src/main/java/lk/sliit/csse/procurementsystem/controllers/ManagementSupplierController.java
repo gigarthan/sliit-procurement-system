decd1d21400c36b3661d08504cb625e510a23566
gigarthan <gigajpx@gmail.com>
2018-10-01 07:11:57 +0530

    add suppliers controller

--- src/main/java/lk/sliit/csse/procurementsystem/controllers/ManagementSupplierController.java
+++ src/main/java/lk/sliit/csse/procurementsystem/controllers/ManagementSupplierController.java
@@ -1,4 +1,36 @@
 package lk.sliit.csse.procurementsystem.controllers;
 
+import lk.sliit.csse.procurementsystem.models.Supplier;
+import lk.sliit.csse.procurementsystem.repositories.SupplierRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.inject.Named;
+import java.util.List;
+
+@Data
+@Named
 public class ManagementSupplierController {
+
+    private Supplier supplier = new Supplier();
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierRepository.findAll();
+    }
+
+    public void addSupplier() {
+        supplier.setPassword(new BCryptPasswordEncoder().encode("1234"));
+        supplier.setBlackListed(false);
+        supplierRepository.save(this.supplier);
+        this.supplier = new Supplier();
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
 }
