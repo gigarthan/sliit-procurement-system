66a360e86ef24674a8d9e97a7ea1469ec72359f9
gigarthan <gigajpx@gmail.com>
2018-10-01 07:15:46 +0530

    add supplier model

--- src/main/java/lk/sliit/csse/procurementsystem/models/Supplier.java
+++ src/main/java/lk/sliit/csse/procurementsystem/models/Supplier.java
@@ -6,13 +6,62 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import java.util.List;
 
-@Data
 @Entity
+@Data
 public class Supplier extends User {
     private String name;
     private String accountNo;
     private String phone;
     private boolean availability;
+    private boolean blackListed;
     @ElementCollection
-    private List<?> items;
+    private List<Items> items;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+    public boolean isBlackListed() {
+        return blackListed;
+    }
+
+    public void setBlackListed(boolean blackListed) {
+        this.blackListed = blackListed;
+    }
+
+    public List<Items> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Items> items) {
+        this.items = items;
+    }
 }
